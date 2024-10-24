3639872b76f57a28e56d1cd52ddb28ab61471398
philippevde <contact@van-der-eecken.eu>
2024-10-24 15:36:01 +0200

    Afficher des données dans un composant

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,29 @@
-import type { FC } from 'react';
+import type { FC } from "react";
+
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+	},
+];
 
 const PokemonCard: FC = () => {
-  return (
-    <figure className="pokemon-card">
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-      <figcaption>Bulbasaur</figcaption>
-    </figure>
-  );
+	const pokemon = pokemonList[0]; // Affecte le premier Pokémon du tableau
+
+	return (
+		<figure className="pokemon-card">
+			{pokemon.imgSrc ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>???</p>
+			)}
+			<figcaption>{pokemon.name}</figcaption>
+		</figure>
+	);
 };
 
 export default PokemonCard;
