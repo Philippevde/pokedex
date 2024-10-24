237dee6b90aa274464077302a54782a9b73fa164
philippevde <contact@van-der-eecken.eu>
2024-10-24 17:30:28 +0200

    Passer des props

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,23 @@ import type { FC } from "react";
 import PokemonCard from "./components/PokemonCard";
 import "./components/PokemonCard.css";
 
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 const App: FC = () => {
-	return (
-		<div className="app">
-			<PokemonCard />
-		</div>
-	);
+    return (
+        <div className="app">
+            <PokemonCard pokemon={pokemonList[0]} />
+        </div>
+    );
 };
 
 export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,29 +1,19 @@
 import type { FC } from "react";
 
-const pokemonList = [
-	{
-		name: "bulbasaur",
-		imgSrc:
-			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-	},
-	{
-		name: "mew",
-	},
-];
+const PokemonCard: FC = ({ pokemon }) => {
+    // Supprime le console.log(props)
+    // Supprime la ligne const pokemon = { name: "mew", imgSrc: null };
 
-const PokemonCard: FC = () => {
-	const pokemon = pokemonList[0]; // Affecte le premier Pokémon du tableau
-
-	return (
-		<figure className="pokemon-card">
-			{pokemon.imgSrc ? (
-				<img src={pokemon.imgSrc} alt={pokemon.name} />
-			) : (
-				<p>???</p>
-			)}
-			<figcaption>{pokemon.name}</figcaption>
-		</figure>
-	);
+    return (
+        <figure className="pokemon-card">
+            {pokemon.imgSrc ? (
+                <img src={pokemon.imgSrc} alt={pokemon.name} />
+            ) : (
+                <p>???</p>
+            )}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    );
 };
 
 export default PokemonCard;
